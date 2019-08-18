dc1b46ed32f6ead4168ff100d947050c2b3fc3ee
robgrig <rapopiap@gmail.com>
2019-08-18 13:37:39 +0400

    asdf

---
+++ lecture1/practical/practical2 _2.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# problem_1 
+# 
+# 
+
+# In[4]:
+
+
+student= input("student")
+
+
+# In[7]:
+
+
+course= input ("course")
+
+
+# In[10]:
+
+
+nickname=input ("nickname")
+
+
+# In[2]:
+
+
+grade=input("grade")
+
+
+# In[5]:
+
+
+print("student", student) 
+
+
+
+# In[8]:
+
+
+print("couse" , course)
+
+
+# In[11]:
+
+
+print("nickname" , nickname)
+
+
+# In[24]:
+
+
+print("grade" , grade)
+
