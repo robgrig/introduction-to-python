7ce4894eea4a3ad621a792de73f6a7adf7ac4124
robgrig <rapopiap@gmail.com>
2019-08-18 13:11:44 +0400

    asdf

---
+++ lecture1/practical/problem1.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# problem_1 
+# 
+# 
+
+# In[7]:
+
+
+student= "Robert Grigoryan"
+
+
+# In[2]:
+
+
+course= "intro to python"
+
+
+# In[19]:
+
+
+nickname="Rob"
+
+
+# In[20]:
+
+
+grade=100
+
+
+# In[22]:
+
+
+pythonexpert=True 
+
+
+# In[12]:
+
+
+print("student", student) 
+
+
+
+# In[13]:
+
+
+print("couse" , course)
+
+
+# In[23]:
+
+
+print("nickname" , nickname)
+
+
+# In[24]:
+
+
+print("grade" , grade)
+
---
+++ lecture1/practical/problem2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[33]:
+
+
+import math
+
+
+# In[34]:
+
+
+AB=3
+
+
+# In[35]:
+
+
+AC=4
+
+
+# In[36]:
+
+
+BC=AC**2+AB**2
+
+
+# In[40]:
+
+
+BC=math.sqrt(BC)
+
+
+# In[41]:
+
+
+print(BC)
+
